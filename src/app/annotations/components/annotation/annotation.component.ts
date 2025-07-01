e85e8b44ad6ceddc8d550842ccca73d47cd867bb
Vitaliy Badalov <badaliki@yandex.ru>
2025-07-01 14:44:11 +0300

    feat(annotations): Added scaling of annotations

--- src/app/annotations/components/annotation/annotation.component.ts
+++ src/app/annotations/components/annotation/annotation.component.ts
@@ -1,7 +1,21 @@
-import { ChangeDetectorRef, Component, ComponentRef, HostListener, inject, input } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ComponentRef,
+  computed,
+  HostListener,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
+import { pairwise } from 'rxjs';
+
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 import { AnnotationsService } from '@annotations/services/annotations.service';
+import { DocumentControlsService } from '@documents/services/document-controls.service';
 
+@UntilDestroy()
 @Component({
   selector: 'app-annotation',
   host: {
@@ -13,7 +27,7 @@ import { AnnotationsService } from '@annotations/services/annotations.service';
   templateUrl: './annotation.component.html',
   styleUrl: './annotation.component.scss',
 })
-export class AnnotationComponent {
+export class AnnotationComponent implements OnInit {
 
   public id = input.required<string>();
 
@@ -33,13 +47,39 @@ export class AnnotationComponent {
 
   private readonly _annotationsService = inject(AnnotationsService);
 
+  private readonly _documentControlsService = inject(DocumentControlsService);
+
   private readonly _changeDetectorRef = inject(ChangeDetectorRef);
+
+  private _ratio = computed(() => parseInt(this.height()) / parseInt(this.width()));
 
   private _startMovingLayerY = 0;
 
   private _startMovingLayerX = 0;
 
   private _startedMovingMatrix: DOMMatrixReadOnly | null = null;
+
+  public ngOnInit(): void {
+    this._documentControlsService.scale$
+      .pipe(
+        pairwise(),
+        untilDestroyed(this),
+      )
+      .subscribe((values) => {
+        const element = this.componentRef().location.nativeElement;
+        const transform = element.style.transform;
+        const matrix = new DOMMatrixReadOnly(transform);
+        const width = parseInt(element.style.width);
+        const ratio = this._ratio();
+        const newTranslateY = matrix.m42 / values[0] * values[1];
+        const newTranslateX = matrix.m41 / values[0] * values[1];
+        const newWidth = width / values[0] * values[1];
+
+        element.style.transform = `translateY(${newTranslateY}px) translateX(${newTranslateX}px)`;
+        element.style.width = `${newWidth}px`;
+        element.style.height = `${newWidth * ratio}px`;
+      });
+  }
 
   protected delete(): void {
     this._annotationsService.delete(this.componentRef());
